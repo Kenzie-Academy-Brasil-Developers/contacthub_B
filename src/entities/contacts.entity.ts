17e5b2f7612ab2ffba40442b64e6f4ef0b7cd791
Astolfo Daniel <128754740+Astolfoastt@users.noreply.github.com>
2024-01-23 22:00:41 -0300

    Feat: arquitetura inicial e relação de entidades

---
+++ src/entities/contacts.entity.ts
@@ -0,0 +1,33 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import User from "./users.entity";
+
+
+@Entity("contacts")
+class Contact {
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+
+  @Column({ length: 45, unique: true })
+  name: string;
+
+  @Column({ length: 45, unique: true })
+  email: string;
+
+  @Column({type: "numeric", precision: 15, scale: 0, unique: true})
+  contactNumber: number;
+
+  @CreateDateColumn({ type: "date" })
+  createdAt: string;
+
+  @ManyToOne(() => User, user => user.contacts)
+    user: User;
+}
+
+
+export default Contact;
---
+++ src/entities/users.entity.ts
@@ -0,0 +1,37 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import Contact from "./contacts.entity";
+
+@Entity("users")
+class User {
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+
+  @Column({ length: 45 })
+  name: string;
+
+  @Column({ length: 45, unique: true })
+  email: string;
+
+  @Column({ type: "boolean", default: false})
+  admin: boolean;
+
+  @Column({ length: 120 })
+  password: string;
+
+  @Column({type: "numeric", precision: 15, scale: 0, unique: true})
+  contactNumber: number;
+
+  @CreateDateColumn({ type: "date" })
+  createdAt: string;
+
+  @OneToMany(() => Contact, (contact) => contact.user)
+  contacts: Contact[]
+}
+
+export default User;
