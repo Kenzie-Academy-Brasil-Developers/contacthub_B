48c5a9e45a05601beb576fd905c8fe93752e40f9
Astolfo Daniel <128754740+Astolfoastt@users.noreply.github.com>
2024-02-08 11:21:51 -0300

    Fix: implementação de CORS para rotas

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,12 @@ import { handleErrors } from "./middlewares/handleErrors";
 import { userRouter } from "./routes/user.routes";
 import { loginRouter } from "./routes/login.routes";
 import { contactRouter } from "./routes/contact.routes";
-
+import cors from "cors"
 
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use("/", loginRouter, userRouter, contactRouter);
 app.use(handleErrors);
