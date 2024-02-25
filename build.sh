03859b23ec8381318155637cbc936c1357b83e65
Astolfo Daniel <128754740+Astolfoastt@users.noreply.github.com>
2024-02-25 09:43:47 -0300

    Fix: alteração na coluna contactNumber para string

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@
 cd src
 tsc
 set -o errexit
-
+psql -U astol -d contacHubDB -c "DELETE FROM contacts;"
+psql -U astol -d contacHubDB -c "DELETE FROM users;"
 yarn
 yarn build
 yarn typeorm migration:run -d dist/data-source
