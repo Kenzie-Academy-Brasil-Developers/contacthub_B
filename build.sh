466fffae02cd992f127786465b5b868c87cd6946
Astolfo Daniel <128754740+Astolfoastt@users.noreply.github.com>
2024-02-25 09:49:35 -0300

    Fix: alteração na coluna contactNumber para string

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ cd src
 tsc
 set -o errexit
 
-yarn DROP TABLES;
+
+psql -U astol -d contactHubDB -c "DO $$ DECLARE r RECORD; BEGIN FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE'; END LOOP; END $$;"
 
 yarn
 yarn build
