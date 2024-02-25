f124ece190acad8603854c2317a4f8e6d5c7c967
Astolfo Daniel <128754740+Astolfoastt@users.noreply.github.com>
2024-02-25 12:19:02 -0300

    Fix: correção das migrations

--- src/migrations/1708874292490-InitialMigration.ts
+++ src/migrations/1708874292490-InitialMigration.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class InitialMigration1708869614464 implements MigrationInterface {
-    name = 'InitialMigration1708869614464'
+export class InitialMigration1708874292490 implements MigrationInterface {
+    name = 'InitialMigration1708874292490'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "contacts" ("id" SERIAL NOT NULL, "name" character varying(45) NOT NULL, "email" character varying(45) NOT NULL, "contactNumber" character varying(15) NOT NULL, "createdAt" date NOT NULL DEFAULT now(), "userId" integer, CONSTRAINT "UQ_82e70145f0669e778561f3877f2" UNIQUE ("name"), CONSTRAINT "UQ_752866c5247ddd34fd05559537d" UNIQUE ("email"), CONSTRAINT "PK_b99cd40cfd66a99f1571f4f72e6" PRIMARY KEY ("id"))`);
