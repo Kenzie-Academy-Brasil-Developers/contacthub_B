fc2fcfc609016ab720e95c5ee73f7fcb43bce5c3
Astolfo Daniel <128754740+Astolfoastt@users.noreply.github.com>
2024-02-25 10:00:19 -0300

    Fix: alteração na coluna contactNumber para string

--- build.sh
+++ build.sh
@@ -4,10 +4,6 @@
 cd src
 tsc
 set -o errexit
-
-
-yarn -U contacthubdb_user -d contacthubdb -c "DO $$ DECLARE r RECORD; BEGIN FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE'; END LOOP; END $$;"
-
 yarn
 yarn build
 yarn typeorm migration:run -d dist/data-source
